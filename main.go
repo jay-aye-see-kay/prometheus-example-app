d261fc463a65222ec5386c9a34b2666933cb05c5
Jack Rose <user@jackrose.co.nz>
2025-04-17 15:21:11 +1000

    add wait and hash handlers so we can monitor slow requests

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -54,9 +59,42 @@ func main() {
 	notfoundHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 	})
-    internalErrorHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.WriteHeader(http.StatusInternalServerError)
-    })
+	internalErrorHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	})
+
+	waitHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		waitSecStr := r.PathValue("waitSec")
+		waitSec, _ := strconv.Atoi(waitSecStr)
+		if waitSec < 1 {
+			waitSec = 5 // errors, no value, and negative values all default to 5 seconds
+		}
+		time.Sleep(time.Duration(waitSec) * time.Second)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Waited for " + waitSecStr + " seconds."))
+	})
+
+	hashHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		iterationsStr := r.PathValue("iterations")
+		iterations, _ := strconv.Atoi(iterationsStr)
+		if iterations < 1 {
+			iterations = 5 // errors, no value, and negative values all default to 5 iterations
+		}
+		mbStr := r.PathValue("mb")
+		mb, _ := strconv.Atoi(mbStr)
+		if mb < 1 {
+			mb = 5 // errors, no value, and negative values all default to 5 mb
+		}
+		fmt.Printf("Hashing %d mb, %d times\n", mb, iterations)
+		start := time.Now()
+		for range iterations {
+			hash := hashRandomData(mb * 1024 * 1024)
+			fmt.Println("completed hash with result: " + hash)
+		}
+		elapsed := time.Since(start)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hashing %d mb, %d times took %s", mb, iterations, elapsed)))
+	})
 
 	foundChain := promhttp.InstrumentHandlerDuration(
 		httpRequestDuration.MustCurryWith(prometheus.Labels{"handler": "found"}),
@@ -67,6 +105,11 @@ func main() {
 	mux.Handle("/", foundChain)
 	mux.Handle("/err", promhttp.InstrumentHandlerCounter(httpRequestsTotal, notfoundHandler))
 	mux.Handle("/internal-err", promhttp.InstrumentHandlerCounter(httpRequestsTotal, internalErrorHandler))
+	mux.Handle("/wait/{waitSec}", promhttp.InstrumentHandlerCounter(httpRequestsTotal, waitHandler))
+	mux.Handle("/wait/", promhttp.InstrumentHandlerCounter(httpRequestsTotal, waitHandler))
+	mux.Handle("/hash/{mb}/{iterations}", promhttp.InstrumentHandlerCounter(httpRequestsTotal, hashHandler))
+	mux.Handle("/hash/{mb}", promhttp.InstrumentHandlerCounter(httpRequestsTotal, hashHandler))
+	mux.Handle("/hash/", promhttp.InstrumentHandlerCounter(httpRequestsTotal, hashHandler))
 	mux.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 
 	var srv *http.Server
@@ -77,4 +120,20 @@ func main() {
 	}
 
 	log.Fatal(srv.ListenAndServe())
+}
+
+func hashRandomData(bytesToProcess int) string {
+	buffer := make([]byte, 1024) // 1KB buffer
+	hasher := sha256.New()
+
+	bytesProcessed := 0
+	h := []byte{}
+	for bytesProcessed < bytesToProcess {
+		rand.Read(buffer) // Fill buffer with random data
+		hasher.Write(buffer)
+		h = hasher.Sum(nil)
+		bytesProcessed += len(buffer)
+	}
+
+	return fmt.Sprintf("%x", h)
 }
